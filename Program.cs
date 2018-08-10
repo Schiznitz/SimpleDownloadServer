c4b95e1e2cfa97512f851a2576eb30600e16d84c
Schiznitz <me@schiznitz.com>
2018-08-10 15:03:04 +0800

    init

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.StaticFiles;
+using Microsoft.Extensions.Configuration;
+
+namespace SimpleDownloadServer
+{
+    public class Program
+    {
+        class Startup
+        {
+            class SimpleContentTypeProvider : IContentTypeProvider
+            {
+                public bool TryGetContentType(string subpath, out string contentType)
+                {
+                    contentType = "application/x-msdownload";
+                    return true;
+                }
+            }
+
+            public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+            {
+                app.UseHsts();
+
+                app.UseHttpsRedirection();
+
+                var opts = new FileServerOptions { EnableDirectoryBrowsing = true };
+                opts.StaticFileOptions.ContentTypeProvider = new SimpleContentTypeProvider();
+
+                app.UseFileServer(opts);
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                   .UseConfiguration(new ConfigurationBuilder().AddCommandLine(args).Build())
+                   .UseStartup<Startup>();
+    }
+}
