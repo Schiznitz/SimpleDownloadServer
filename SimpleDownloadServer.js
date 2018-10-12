2dbd39bbf96d6b71b328fbded76e5ee4ffd3afd6
Jim Chan <me@schiznitz.com>
2018-10-12 10:58:53 +0800

    Update SimpleDownloadServer.js

--- SimpleDownloadServer.js
+++ SimpleDownloadServer.js
@@ -1,5 +1,5 @@
 const spawn = require('child_process').spawn;
-const dotnet = spawn('dotnet', ['run', '--urls', 'http://*:5000']);
+const dotnet = spawn('dotnet', ['run', '--urls', 'http://*:5000'], { cwd: __dirname });
 
 dotnet.stdout.pipe(process.stdout);
 dotnet.stderr.pipe(process.stderr);
